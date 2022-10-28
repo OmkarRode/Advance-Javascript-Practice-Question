26c2f7dc7f32b05036f99c6f7a610d125bd51b12
Omkar Rode <rodeomkar5@gmail.com>
2022-10-29 00:16:23 +0530

    bind method

--- main.js
+++ main.js
@@ -5,5 +5,8 @@ var addnum=function(a,b,c)
     return this.num+a+b+c;
 }
 // console.log(addnum.call(obj,3));
-var arr=[1,3,4];
-console.log(addnum.apply(obj,arr));
+// var arr=[1,3,4];
+// console.log(addnum.apply(obj,arr));
+
+var bound=addnum.bind(obj);
+console.log(bound(2,4,5));
