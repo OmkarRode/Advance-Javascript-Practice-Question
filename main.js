8000a1a1076b7a0415f21f301ddab5bd00ea1cb8
Omkar Rode <rodeomkar5@gmail.com>
2022-10-29 00:20:00 +0530

    student age print

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
-var obj={num:10}
+// var obj={num:10}
 
-var addnum=function(a,b,c)
+// var addnum=function(a,b,c)
+// {
+//     return this.num+a+b+c;
+// }
+// // console.log(addnum.call(obj,3));
+// // var arr=[1,3,4];
+// // console.log(addnum.apply(obj,arr));
+
+// var bound=addnum.bind(obj);
+// console.log(bound(2,4,5));
+
+var student={age:20};
+var readAge=function()
 {
-    return this.num+a+b+c;
+    return this.age;
 }
-// console.log(addnum.call(obj,3));
-// var arr=[1,3,4];
-// console.log(addnum.apply(obj,arr));
 
-var bound=addnum.bind(obj);
-console.log(bound(2,4,5));
+console.log(readAge.call(student));
