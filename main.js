8fd493e7c8d74f638e3ae62dd0b21be3ba716300
Omkar Rode <rodeomkar5@gmail.com>
2022-10-29 00:14:41 +0530

    applay method

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 var obj={num:10}
 
-var addnum=function(a)
+var addnum=function(a,b,c)
 {
-    return this.num+a;
+    return this.num+a+b+c;
 }
-console.log(addnum.call(obj,3));
+// console.log(addnum.call(obj,3));
+var arr=[1,3,4];
+console.log(addnum.apply(obj,arr));
