ba89406b4e6597296087b8ee1e7f2fae4192a623
Omkar Rode <rodeomkar5@gmail.com>
2022-10-29 00:32:28 +0530

    anther carring method

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
-let multiply=function(a,b)
+// let multiply=function(a,b)
+// {
+//     return a*b;
+// }
+
+
+// let muliplyByTwo=multiply.bind(this,2);
+
+// console.log(muliplyByTwo(5));
+// let muliplyByThree=multiply.bind(this,3);
+// console.log(muliplyByThree(8));
+
+
+let multiply=function(a)
 {
-    return a*b;
+    return function(b){
+        console.log(a*b);
+    };
 }
-
-let muliplyByTwo=multiply.bind(this,2);
-
-console.log(muliplyByTwo(5));
-let muliplyByThree=multiply.bind(this,3);
-console.log(muliplyByThree(8));
-
-
+let muliplyByTwo=multiply(5);
+muliplyByTwo(8);
